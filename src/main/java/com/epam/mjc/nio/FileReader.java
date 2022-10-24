c9efc447e5acccf774cbab29fc2603f9407522d1
m-salakhedenov <m.salakhedenov@gmail.com>
2022-10-24 16:03:37 +0600

    implemented file reading logic

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,43 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String rawData = getRawData(file);
+
+        return null;
     }
+
+    private String getRawData(File file) {
+        try (
+                FileInputStream fileInputStream = new FileInputStream(file);
+                FileChannel channel = fileInputStream.getChannel()
+        ) {
+            ByteBuffer buffer = ByteBuffer.allocate(48);
+            StringBuilder rawData = new StringBuilder();
+
+            while (channel.read(buffer) > 0) {
+                buffer.flip();
+                for (int i = 0; i < buffer.limit(); i++) {
+                    rawData.append((char) buffer.get(i));
+                }
+                buffer.clear();
+            }
+
+            return rawData.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 }
